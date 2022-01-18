4309d050e16610e886cbb7dec86f28fe245a3474
federicodonner <federico.donner@gmail.com>
2022-01-18 14:03:51 -0300

    fix(login): remove the option to create users

--- src/login/Login.js
+++ src/login/Login.js
@@ -13,19 +13,8 @@ export default function Login() {
   const [usernameError, setUsernameError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
-  const [createUsernameError, setCreateUsernameError] = useState(false);
-  const [createNameError, setCreateNameError] = useState(false);
-  const [createEmailError, setCreateEmailError] = useState(false);
-  const [createPasswordError, setCreatePasswordError] = useState(false);
-
-  const [creatingAccount, setCreatingAccount] = useState(false);
-
   const loginUsername = useRef(null);
   const loginPassword = useRef(null);
-  const createUsername = useRef(null);
-  const createName = useRef(null);
-  const createEmail = useRef(null);
-  const createPassword = useRef(null);
 
   let navigate = useNavigate();
 
@@ -92,62 +81,6 @@ export default function Login() {
     );
   }
 
-  function createUser(e) {
-    // Prevent navigation on form submit
-    e.preventDefault();
-
-    // Verifies that the user enterd the information
-    if (!createUsername.current.value) {
-      setCreateUsernameError(true);
-    }
-    if (!createName.current.value) {
-      setCreateNameError(true);
-    }
-    if (!createEmail.current.value) {
-      setCreateEmailError(true);
-    }
-    if (!createPassword.current.value) {
-      setCreatePasswordError(true);
-    }
-
-    if (
-      !createUsername.current.value ||
-      !createName.current.value ||
-      !createEmail.current.value ||
-      !createPassword.current.value
-    ) {
-      return false;
-    }
-    // If there is a username and a password, send it to the API
-    var createUsernameData = createUsername.current.value;
-    var createNameData = createName.current.value;
-    var createEmailData = createEmail.current.value;
-    setLoginLoader(true);
-    accessAPI(
-      "POST",
-      "user",
-      JSON.stringify({
-        username: createUsernameData,
-        name: createNameData,
-        email: createEmailData,
-        password: createPassword.current.value,
-      }),
-      (response) => {
-        // If the login is successful, store the token in LS and navigate
-        storeInLS(process.env.REACT_APP_LS_LOGIN_TOKEN, response.token);
-        navigate("/home");
-      },
-      (response) => {
-        setLoginLoader(false);
-        // Restores the data entered
-        createUsername.current.value = createUsernameData;
-        createName.current.value = createNameData;
-        createEmail.current.value = createEmailData;
-        alert(response.message);
-      }
-    );
-  }
-
   return (
     <div>
       <Header showMenu={false} />
@@ -157,7 +90,7 @@ export default function Login() {
             <img src={whiteLoader} className="loader" alt="Loading" />
           </div>
         )}
-        {!loginLoader && !creatingAccount && (
+        {!loginLoader && (
           <div className="loginForm">
             <div className="fields">
               <form onSubmit={loginUser}>
@@ -185,71 +118,7 @@ export default function Login() {
                 </button>
               </form>
             </div>
-            <div className="textButton light">{texts.FORGOT_PASSWORD}</div>
-            <div className="divider light"></div>
-            <button
-              className="light create"
-              onClick={() => {
-                setCreatingAccount(true);
-              }}
-            >
-              {texts.CREATE_ACCOUNT}
-            </button>
-          </div>
-        )}
-        {!loginLoader && creatingAccount && (
-          <div className="loginForm">
-            <div className="fields">
-              <form onSubmit={createUser}>
-                <input
-                  type="text"
-                  placeholder={texts.CREATE_USERNAME}
-                  className={createUsernameError ? "error" : ""}
-                  onChange={() => {
-                    setCreateUsernameError(false);
-                  }}
-                  ref={createUsername}
-                />
-                <input
-                  type="text"
-                  placeholder={texts.CREATE_NAME}
-                  className={createNameError ? "error" : ""}
-                  onChange={() => {
-                    setCreateNameError(false);
-                  }}
-                  ref={createName}
-                />
-                <input
-                  type="text"
-                  placeholder={texts.CREATE_EMAIL}
-                  className={createEmailError ? "error" : ""}
-                  onChange={() => {
-                    setCreateEmailError(false);
-                  }}
-                  ref={createEmail}
-                />
-                <input
-                  type="password"
-                  placeholder={texts.CREATE_PASSWORD}
-                  className={createPasswordError ? "error" : ""}
-                  onChange={() => {
-                    setCreatePasswordError(false);
-                  }}
-                  ref={createPassword}
-                />
-                <button className="light register" type="submit">
-                  {texts.CREATE_ACCOUNT}
-                </button>{" "}
-              </form>
-              <button
-                className="light register"
-                onClick={() => {
-                  setCreatingAccount(false);
-                }}
-              >
-                {texts.CANCEL}
-              </button>
-            </div>
+            <div className="textButton white">{texts.FORGOT_PASSWORD}</div>
           </div>
         )}
       </div>
